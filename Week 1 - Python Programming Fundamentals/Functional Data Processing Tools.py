774dfa4f965be4c33c3b1f7391b546890742cf47
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-05-27 15:46:44 +0530

    Update Functional Data Processing Tools.py

--- Week 1 - Python Programming Fundamentals/Functional Data Processing Tools.py
+++ Week 1 - Python Programming Fundamentals/Functional Data Processing Tools.py
@@ -10,4 +10,11 @@ print(avg(3,5,10))            #6.0
 print(app(lambda x: x*2, 2))  #10
 
 # Mapping
+def cube(l):
+  return l*l*l
+
+l = [1,4,2,6,7,8]
+newl = []
+newl = list((map(cube,l)))
+print(newl)
 
