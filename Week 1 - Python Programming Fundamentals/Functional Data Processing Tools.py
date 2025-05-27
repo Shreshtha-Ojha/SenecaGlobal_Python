460fda4566c3f814c0ed12f812e0b15d067d12b6
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-05-27 17:04:34 +0530

    Update Functional Data Processing Tools.py

--- Week 1 - Python Programming Fundamentals/Functional Data Processing Tools.py
+++ Week 1 - Python Programming Fundamentals/Functional Data Processing Tools.py
@@ -18,3 +18,39 @@ newl = []
 newl = list((map(cube,l)))
 print(newl)
 
+#Filter
+def check(a):
+  return a>4
+l = [1,4,8,2,0,9,-9]  
+newl = list(filter(check,l))  
+print(newl)
+
+# Regex
+import re
+
+pattern = r"[A-Z]+nglish"
+text = '''The English Wikipedia is the primary[a] English -language edition of Wikipedia,
+ an online encyclopedia. It was Vnglish created by Jimmy Wales and Larry Sanger on 15 January 
+ 2001, as Wikipedia's first edition.
+
+English Wikipedia is hosted alongside other language editions by the Wikimedia
+ Foundation, an American nonprofit organization. Its content, written independently 
+ of other editions by volunteer editors Vnglish known as Wikipedians,[1] is in various 
+ varieties of English while aiming to stay consistent within articles. 
+ Its internal newspaper is The Signpost.'''
+
+matches = re.finditer(pattern , text)
+for match in matches:
+  print(match)
+
+
+''' Output
+<re.Match object; span=(4, 11), match='English'>
+<re.Match object; span=(40, 47), match='English'>
+<re.Match object; span=(112, 119), match='Vnglish'>
+<re.Match object; span=(213, 220), match='English'>
+<re.Match object; span=(417, 424), match='Vnglish'>
+<re.Match object; span=(479, 486), match='English'>'''
+
+
+
