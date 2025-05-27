083540dde4deeb650a8215c3a50582f6a4768f77
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-05-27 15:31:03 +0530

    Create Functional Data Processing Tools.py

---
+++ Week 1 - Python Programming Fundamentals/Functional Data Processing Tools.py
@@ -0,0 +1,13 @@
+# Lambda
+
+def app(fx , value):
+  return 6 +fx(value)
+
+square = lambda x: x*x
+avg = lambda x,y,z: (x+y+z)/3
+print(square(5))              # 25
+print(avg(3,5,10))            #6.0
+print(app(lambda x: x*2, 2))  #10
+
+# Mapping
+
