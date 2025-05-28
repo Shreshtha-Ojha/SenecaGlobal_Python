2f82fe343cf0ebb0ea396ef9a9c963bb2abc6a3c
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-05-28 17:53:33 +0530

    Update Data Structures.py

--- Week 1 - Python Programming Fundamentals/Data Structures.py
+++ Week 1 - Python Programming Fundamentals/Data Structures.py
@@ -1,1 +1,151 @@
+#Arrays
+'''Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+Note that you must do this in-place without making a copy of the array.'''
+class Solution:
+    def moveZeroes(self, nums):
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+
+
+'''Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.'''
+
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        k=1
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[k-1]:
+                nums[k]=nums[i]
+                k+=1
+        return k   
+
+
+'''You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.'''
+class Solution(object):
+    def maxProfit(self, prices):
+      max_p = 0
+      min_p = float('inf')
+
+      for p in prices:
+        if p<min_p:
+            min_p = p
+        else:
+            p = p-min_p
+            max_p = max(max_p , p)    
+      return max_p      
+
+
+'''Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.'''
+class Solution(object):
+    def sortedSquares(self, nums):
+        sq = []
+        for i in nums:
+           sq.append(i**2)
+        return sorted(sq)   
+
+
+'''Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.'''
+
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+        n = len(nums)
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left, right = i + 1, n - 1
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+                if s < 0:
+                    left += 1
+                elif s > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1  
+        return res
+
+
+#String Manipulation
+'''Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.'''
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        map_s_to_t = {}
+        map_t_to_s = {}
+
+        for char_s, char_t in zip(s, t):
+            if char_s in map_s_to_t:
+                if map_s_to_t[char_s] != char_t:
+                    return False
+            else:
+                map_s_to_t[char_s] = char_t
+
+            if char_t in map_t_to_s:
+                if map_t_to_s[char_t] != char_s:
+                    return False
+            else:
+                map_t_to_s[char_t] = char_s
+
+        return True
+
+
+'''You are given a string s. You can convert s to a palindrome by adding characters in front of it.
+
+Return the shortest palindrome you can find by performing this transformation.
+
+ '''
+class Solution(object):
+    def shortestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s or s == s[::-1]:
+            return s
+        
+        temp = s + "#" + s[::-1]
+
+        lps = [0] * len(temp)
+        for i in range(1, len(temp)):
+            j = lps[i - 1]
+            while j > 0 and temp[i] != temp[j]:
+                j = lps[j - 1]
+            if temp[i] == temp[j]:
+                j += 1
+            lps[i] = j
+        to_add = s[lps[-1]:][::-1]  
+        return to_add + s
+
+           
