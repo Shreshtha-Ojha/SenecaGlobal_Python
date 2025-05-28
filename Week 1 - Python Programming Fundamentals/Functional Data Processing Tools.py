62c445fa8c209b68b031affbd2d9e6374891c00c
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-05-28 12:01:01 +0530

    Update Functional Data Processing Tools.py

--- Week 1 - Python Programming Fundamentals/Functional Data Processing Tools.py
+++ Week 1 - Python Programming Fundamentals/Functional Data Processing Tools.py
@@ -56,6 +56,38 @@ for match in matches:
 <re.Match object; span=(479, 486), match='English'>'''
 
 
+#File operations
+#open a file
+f = open("abc.txt" , "w")
+f = open("abc.txt")
+f.read()                  #output is an empty string
+
+f = open("abc.txt" , 'w')
+f.write("Lorem Ipsum is simply dummy text of the printing \n and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it\n to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset \n sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including \n versions of Lorem Ipsum.")
+f.close()
+
+f = open("abc.txt")
+print(f.readline())  #Output only one line
+print(f.readlines()) #Output is all the lines as different elements in the list
+print(f.read())      #output: The whole paragraph
+f.close()
+# use of OS module
+import os
+os.path.exists("abc.txt")
+try:
+    file = open("newfile.txt", "x")  # Fails if file exists
+except FileExistsError:
+    print("File already exists")
+
+print(os.path.getsize("abc.txt"))      # Size in bytes   output 581
+print(os.path.abspath("abc.txt"))      # Absolute path   output  /content/abc.txt
+print(os.path.basename("path/abc.txt"))# File name       output  abc.txt
+
+
+os.rename('abc.txt', 'newfile.txt')   # Rename file
+
+
+
 
 
 
