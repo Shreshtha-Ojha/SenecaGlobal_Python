863e082ddb14a79a7f38162f7632d47fd52acdc1
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-06-01 13:17:51 +0530

    Update Data Structures.py

--- Week 1 - Python Programming Fundamentals/Data Structures.py
+++ Week 1 - Python Programming Fundamentals/Data Structures.py
@@ -403,43 +403,6 @@ def merge(left, right):
     return merged
 
 
-#Exception Handling
-try:
-    result = 10 / 0
-except ZeroDivisionError:
-    print("Error: Cannot divide by zero.")
-
-try:
-    number = int("abc")
-except ValueError:
-    print("Error: Invalid literal for int().")
-except TypeError:
-    print("Error: Type mismatch.")
-
-try:
-    result = 10 / 2
-except ZeroDivisionError:
-    print("Error: Cannot divide by zero.")
-else:
-    print(f"Result is {result}")
-finally:
-    print("Execution completed.")
-
-#Custom exceptions
-class CustomError(Exception):
-    pass
-
-try:
-    raise CustomError("This is a custom error.")
-except CustomError as e:
-    print(f"Caught an exception: {e}")
-
-#File not found error
-try:
-    with open("nonexistent_file.txt", "r") as file:
-        content = file.read()
-except FileNotFoundError:
-    print("Error: File not found.")
 
 
 
