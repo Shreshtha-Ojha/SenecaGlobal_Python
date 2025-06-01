67328c0a9783cac48330cfbfab053b41054f544f
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-06-01 23:31:59 +0530

    Update Data Handling.py

--- Week 1 - Python Programming Fundamentals/Data Handling.py
+++ Week 1 - Python Programming Fundamentals/Data Handling.py
@@ -30,9 +30,42 @@ try:
 except CustomError as e:
     print(f"Caught an exception: {e}")
 
-#File not found error
+
+#Serialization/Deserialization
+
+import pickle
+data = {'name': 'Alice', 'age': 25, 'is_active': True}
+with open('data.pkl', 'wb') as f:
+    pickle.dump(data, f)
+with open('data.pkl', 'rb') as f:
+    loaded_data = pickle.load(f)
+
+print(loaded_data)
 try:
     with open("nonexistent_file.txt", "r") as file:
         content = file.read()
 except FileNotFoundError:
     print("Error: File not found.")
+
+import json
+data = {'name': 'Shreshtha', 'age': 30, 'skills': ['Python', 'Data Science']}
+json_str = json.dumps(data)
+
+# Deserialize 
+data_back = json.loads(json_str)
+
+print(json_str)
+print(data_back)
+
+import yaml
+
+data = {'name': 'Ojha', 'age': 2, 'languages': ['English', 'Hindi']}
+
+# Serialize
+yaml_str = yaml.dump(data)
+
+# Deserialize 
+data_back = yaml.safe_load(yaml_str)
+
+print(yaml_str)
+print(data_back)
