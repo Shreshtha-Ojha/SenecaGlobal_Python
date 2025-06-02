8ce36859e0a013c6035bad28fa7c5db1a2b2eb4d
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-06-02 17:40:11 +0530

    Update OO Concepts.py

--- Week 1 - Python Programming Fundamentals/OO Concepts.py
+++ Week 1 - Python Programming Fundamentals/OO Concepts.py
@@ -62,5 +62,17 @@ d = Dog()
 d.speak()                      # Output: Dog barks
 
 
+#Polymorphism
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+d = Dog()
+d.speak()  # Output: Dog barks
+
 
 
