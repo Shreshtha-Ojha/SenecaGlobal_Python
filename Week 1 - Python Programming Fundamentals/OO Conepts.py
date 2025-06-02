738a94362b699a35012faef79602e3dd5e9522bc
Shreshtha-Ojha <148896860+Shreshtha-Ojha@users.noreply.github.com>
2025-06-02 17:29:16 +0530

    Update OO Conepts.py

--- Week 1 - Python Programming Fundamentals/OO Conepts.py
+++ Week 1 - Python Programming Fundamentals/OO Conepts.py
@@ -12,4 +12,55 @@ c2 = car("Camry" , 2024)
 c1.show()                #output: The name of the model is Corolla ,and was launched in the year 2020
 c2.show()                #output: The name of the model is Camry ,and was launched in the year 2024
 
+#Inheritance
+
+class parent:
+    def noise(self):
+        print("Animal speaks to communicate")
+
+class Dog(parent):
+    def bark(self):
+        print("Dog barks")
+d = Dog()
+d.noise() 
+d.bark()  
+
+#output
+#Animal speaks to communicate
+#Dog barks
+#Using the Super() function
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        print(f"Name: {self.name}")
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)  
+        self.breed = breed
+
+    def info(self):
+        super().info()
+        print(f"Breed: {self.breed}")
+
+d = Dog("Leo", "Golden Retriever")
+d.info()
+
+
+#Method Overriding
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+d = Dog()
+d.speak()                      # Output: Dog barks
+
+
+
 
